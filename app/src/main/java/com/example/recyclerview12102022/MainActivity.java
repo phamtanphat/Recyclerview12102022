c30130efcce6d9bd42c6d261e8cc7487b98cd9db
Phat <phatdroid94@gmail.com>
2022-11-16 19:14:54 +0700

    setup up rcv

--- app/src/main/java/com/example/recyclerview12102022/MainActivity.java
+++ app/src/main/java/com/example/recyclerview12102022/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.recyclerview12102022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rcvStore;
+    StoreAdapter storeAdapter;
+    List<Store> storeList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rcvStore = findViewById(R.id.recycler_view_store);
+        storeList = Store.getMock();
+        storeAdapter = new StoreAdapter(storeList);
+        rcvStore.setAdapter(storeAdapter);
     }
 }
